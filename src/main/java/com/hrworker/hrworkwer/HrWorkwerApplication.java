36b7f7a070278fd17e01c646b0f9c6a92ac5978a
Amanda Lourenco <amandalourenco67@gmail.com>
2021-02-06 14:44:55 -0300

    Primeiro commit

---
+++ src/main/java/com/hrworker/hrworkwer/HrWorkwerApplication.java
@@ -0,0 +1,13 @@
+package com.hrworker.hrworkwer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrWorkwerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrWorkwerApplication.class, args);
+	}
+
+}
